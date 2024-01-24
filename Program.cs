d32e2e9e79c113e60ac98d2f9ab2fce4e9866a7e
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:45:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vehicle car1 = new vehicle();
+            corsa cars2 = new corsa();
+
+            Console.WriteLine(cars2.wheels);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+    internal class vehicle
+    {
+        public int wheels = 4;
+
+    }
+    class corsa : vehicle
+    {
+        public int sparewheels = 1;
+    }
+}
